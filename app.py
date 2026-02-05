4a8b09196cccee6018de206a104af44418b0ae65
usercoding12 <rajveerbsingh06@gmail.com>
2026-02-05 19:55:46 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -77,5 +77,8 @@ def index():
 if __name__ == "__main__":
     app.run(debug=True)
     
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
