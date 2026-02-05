6b61b43c3ea0826516203aca19d3ea6154b4b649
usercoding12 <rajveerbsingh06@gmail.com>
2026-02-05 20:27:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -73,12 +73,11 @@ def index():
         }
 
     return render_template("index.html", result=result, suggestions=suggestions)
-
-if __name__ == "__main__":
-    app.run(debug=True)
     
 import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
+
